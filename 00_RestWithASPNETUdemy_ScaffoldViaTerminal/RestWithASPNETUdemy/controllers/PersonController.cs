1a071a472c6e694ef370240e2650fb6ea17db0f8
Ewerton Soares Da Silva <Ewerton.SoaresDaSilva-ext@ab-Inbev.com>
2024-11-21 08:33:08 -0300

    add new cotroller to work with other verbs

---
+++ 00_RestWithASPNETUdemy_ScaffoldViaTerminal/RestWithASPNETUdemy/controllers/PersonController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Inalid input");
+        }
+
+    }
+}
